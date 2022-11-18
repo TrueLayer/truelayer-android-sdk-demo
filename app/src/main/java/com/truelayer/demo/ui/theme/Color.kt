111d7799f330b322249e696df375d5a70aff7869
Keenen Charles <keenen.charles@truelayer.com>
2022-11-18 16:04:27 +0000

    Updated theme

--- app/src/main/java/com/truelayer/demo/ui/theme/Color.kt
+++ app/src/main/java/com/truelayer/demo/ui/theme/Color.kt
@@ -2,6 +2,6 @@ package com.truelayer.demo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Primary = Color(0xFF42488E)
-val PrimaryDark = Color(0xFF6466A3)
+val Primary = Color(0xFF058ed8)
+val PrimaryDark = Color(0xFF066da5)
 val Secondary = Color(0xFFF7AB1B)
