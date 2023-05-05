4c6b9f32227da344cb4b9cd20a6210d091d3de59
Keenen Charles <101184687+tl-keenen-charles@users.noreply.github.com>
2023-05-05 11:11:54 +0100

    PX-324 Release SDK 2.4.0 (#10)
    Integrates the new version of the SDK

--- app/src/main/java/com/truelayer/demo/ui/theme/Color.kt
+++ app/src/main/java/com/truelayer/demo/ui/theme/Color.kt
@@ -2,8 +2,8 @@ package com.truelayer.demo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Primary = Color(0xFF058ed8)
-val PrimaryDark = Color(0xFF066da5)
+val Primary = Color(0xFFF33B92)
+val PrimaryDark = Color(0xFF1F1F25)
 val SurfaceVariant = Color(0xFFdee3eb)
 val SurfaceVariantDark = Color(0xFF42474e)
 val Secondary = Color(0xFFF7AB1B)
